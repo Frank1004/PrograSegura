ce01766813fa261e5e1085cbf607a00d96803d53
Frank1004 <34372632+Frank1004@users.noreply.github.com>
2020-03-26 18:11:16 -0600

    Update curp.py

--- curp.py
+++ curp.py
@@ -1,19 +1,19 @@
 import re
 import sys
 
-def Validar_Curp(curp):
+def ValidC(curp):
     valido = curp
     if re.match('^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$',valido):
-        print('curp valida')
+        print('La Curp es Correcta')
     else:
-        print('curp no valido')
+        print('La Curp no es correcta')
 
 
 
 if __name__ == '__main__':
     curp = sys.argv[1]
     
-    Validar_Curp(curp)
+    ValidC(curp)
 
     
 
